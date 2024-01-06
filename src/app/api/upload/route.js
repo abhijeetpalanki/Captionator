2b3b68ed5839639e91fea6bd10b9b743439d66ff
abhijeetpalanki <ajpalanki@gmail.com>
2024-01-05 20:38:02 -0600

    file upload functionality

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,9 +1,33 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import uniqid from "uniqid";
+
 export async function POST(req) {
   const formData = await req.formData();
   const file = formData.get("file");
-  //   const { name, type } = file;
+  const { name, type } = file;
+  const data = await file.arrayBuffer();
 
-  console.log(file);
+  const s3client = new S3Client({
+    region: "us-east-2",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
-  return Response.json(file);
+  const id = uniqid();
+  const ext = name.split(".").slice(-1)[0];
+  const newName = id + "." + ext;
+
+  const uploadCommand = new PutObjectCommand({
+    Bucket: process.env.BUCKET_NAME,
+    Body: data,
+    ACL: "public-read",
+    ContentType: type,
+    Key: newName,
+  });
+
+  await s3client.send(uploadCommand);
+
+  return Response.json({ name, ext, newName, id });
 }
